890d50528d37cfae30263f7971837eb65e82c746
arth-inukai <inukai@arith-metic.jp>
2016-02-12 14:28:56 +0900

    SetBuildInfo

--- Assets/Eidtor/BatchBuild.cs
+++ Assets/Eidtor/BatchBuild.cs
@@ -8,16 +8,57 @@ public class BatchBuild : EditorWindow{
 	// ビルド対象のシーン
 	//private static string[] scene = new string[]{"Assets/Scenes/test.unity"};
 
-	private static string android_package_name = "Wow00.apk";
-	
+	private static string temp_file_name			= "Wow00.apk";
+
+	private static string bundle_version			= "0.0.0";
+	private static string bundle_code				= "0";
+		
 	// keystore Path
-	private static string keystorePath = "Android/release.keystore";
+	private static string and_keystore_file_path	= "Android/release.keystore";
 	
 	// keystoreのパスワードはUnityEditorで設定できるが保持されないのでここに記述
-	private static string keystorePass = "arith2133";
-	private static string keyaliasPass = "arith2133";
+	private static string and_keystore_pass 		= "arith2133";
+	private static string and_keystore_alias_pass 	= "arith2133";
 
-	private static string log_string	= "********** ";
+	private static string log_string				= "********** ";
+
+
+	public static void SetBuildInfo() {
+
+		Debug.Log(log_string + "SetBuildInfo");
+
+		var commands = System.Environment.GetCommandLineArgs ();
+		string pTmp = "";
+		string[] pTmps = null;
+		for (int i = 0; i < commands.Length; i++) {
+			pTmp = commands [i];
+			pTmps = pTmp.Split ('=');
+			if (pTmps.Length > 1) {
+				switch (pTmps [0]) {
+				case "temp_file_name":
+					temp_file_name = pTmps [1];
+					break;
+				case "bundle_version":
+					bundle_version = pTmps [1];
+					break;
+				case "bundle_code":
+					bundle_code = pTmps [1];
+					break;
+				case "and_keystore_file_path":
+					and_keystore_file_path = pTmps [1];
+					break;
+				case "and_keystore_pass":
+					and_keystore_pass = pTmps [1];
+					break;
+				case "and_keystore_alias_pass":
+					and_keystore_alias_pass = pTmps [1];
+					break;
+				}
+			}
+
+			Debug.Log (log_string + string.Format("command i = {0}, value = {1}", i, pTmp));
+		}
+	}
 
 
 	// ビルド対象のシーンの取得
@@ -46,11 +87,6 @@ public class BatchBuild : EditorWindow{
 	public static void DevelopmentBuild(){
 		
 		Debug.Log(log_string + "DevelopmentBuild");
-
-		var commands = System.Environment.GetCommandLineArgs ();
-		for (int i = 0; i < commands.Length; i++) {
-			Debug.Log (log_string + string.Format("command i = {0}, value = {1}", i, commands [i]));
-		}
 		
 		//if ( BuildiOS(false)==false ) EditorApplication.Exit(1);
 		
@@ -104,20 +140,20 @@ public class BatchBuild : EditorWindow{
 		
 		// keystoreファイルのの場所を設定
 		string keystoreName =
-			System.IO.Directory.GetCurrentDirectory()+"/"+ keystorePath;
+			System.IO.Directory.GetCurrentDirectory()+"/"+ and_keystore_file_path;
 		
 		// set keystoreName
 		PlayerSettings.Android.keystoreName = keystoreName;
 		
 		// パスワードの再設定
-		PlayerSettings.Android.keystorePass = keystorePass;
+		PlayerSettings.Android.keystorePass = and_keystore_pass;
 		
 		// パスワードの再設定
-		PlayerSettings.Android.keyaliasPass = keyaliasPass;
+		PlayerSettings.Android.keyaliasPass = and_keystore_alias_pass;
 
 		// ビルド
 		// シーン、出力ファイル（フォルダ）、ターゲット、オプションを指定
-		string errorMsg = BuildPipeline.BuildPlayer(GetScenes(), android_package_name, BuildTarget.Android, opt);
+		string errorMsg = BuildPipeline.BuildPlayer(GetScenes(), temp_file_name, BuildTarget.Android, opt);
 		
 		// errorMsgがない場合は成功
 		if ( string.IsNullOrEmpty(errorMsg) ){
